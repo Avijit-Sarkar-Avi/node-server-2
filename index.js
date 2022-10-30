e9ac3c9c80244be433ecbbc186b56218f64a23b8
Avijit-Sarkar-Avi <avijit215054@gmail.com>
2022-10-30 11:17:18 +0600

    connect your application with mongodb and set username and password in there

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 //set middleware
 app.use(cors());
 app.use(express.json());
+
+//user: dbuser2
+//pass: CA3MCXZdkkIrjHp5
+
+
+
+const uri = "mongodb+srv://dbuser2:CA3MCXZdkkIrjHp5@cluster0.bkopbwy.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const userCollection = client.db('nodeMongoCrud').collection('users');
+        const user = {
+            name: 'testing test',
+            email: 'testing@gmail.com'
+        }
+        const result = await userCollection.insertOne(user);
+        console.log(result);
+    }
+    finally {
+
+    }
+}
+
+run().catch(error => console.log(error));
 
 app.get('/', (req, res) => {
     res.send('Hello from node mongo server');
